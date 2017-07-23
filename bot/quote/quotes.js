4a5243b288eccefb56c8b565fc7c2bc2d5c47365
Akshay <akshaykulkarni.2104@gmail.com>
2017-07-23 18:16:48 +0530

    added required files

---
+++ bot/quote/quotes.js
@@ -0,0 +1,17 @@
+module.exports = {
+    objects: {
+        'A4':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'cd':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'photo':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'camera':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'phone':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'book':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'magazine':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'clothes':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'laptop':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'dvd':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'tv':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"30.95","World":"38.95"},
+        'printer':{"UK":"7.95","EuropeEU":"26.95","EuropeNonEU":"28.95","USA":"109.95","World":"172.95"},
+        'computer':{"UK":"23.95","EuropeEU":"105.95","EuropeNonEU":"119.95","USA":"125.95","World":"199.95"},
+    }
+}
--- bot/server.js
+++ bot/server.js
@@ -1,6 +1,12 @@
 var restify = require('restify')
 var builder = require('botbuilder')
 
+//=================================================
+// Import external modules
+//=================================================
+var misc     = require('./smalltalk/misc.js');
+var quotes   = require('./quote/getquote.js');
+
 //Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
@@ -13,17 +19,18 @@ var connector = new builder.ChatConnector({
     appPassword:'zgR9gaMeLGJkNhoajkoEN45'
 });
 
+// Create bot
 // Listen for messages from users
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Recieve Messages from the user and respond by echoing each message back 
-var bot = new builder.UniversalBot(connector, [
-    function(session){
-        var msg = new builder.Message(session)
-            .attachments([{
-                contentType: "image/gif",
-                contentUrl: "https://media.giphy.com/media/l41lTR6jmXXxHDgxG/giphy.gif"
-            }]);
-        session.endDialog(msg);
-    }
-]);
+var recognizer = new apiairecognizer('ea5a2c0743924810bb21cc1e2f79a542');
+var intents = new builder.IntentDialog({
+	recognizers: [recognizer]
+});
+
+bot.dialog('/', intents);
+
+
+// Recieve Messages from the user and respond by handling the intent
+intents.matches('smalltalk.greetings', misc.smalltalk);
